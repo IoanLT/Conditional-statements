39a23c3e66e5113635d8742069adafc030c161a7
IoanLT <ioan.tranole@gmail.com>
2020-10-20 20:44:38 +0100

    Added the Conditional statements challenge from WCS

---
+++ main.js
@@ -0,0 +1,14 @@
+let num1 = 1;
+let num2 = 7;
+let num3 = 7;
+
+if(num1 === num2 && num1 === num3 && num2 === num3){
+  console.log('The three variables are the same');
+}
+else if(num1 === num2 || num1 === num3 || num2 === num3){
+  console.log('Two of the variables are the same');
+}
+else {
+  console.log('all variables are different');
+}
+
